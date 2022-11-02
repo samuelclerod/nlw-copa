4b27e94b163a9ca0b4c2b07beee2b0e3d0b3068c
Samuel Rodrigues <samuelclerod@gmail.com>
2022-11-02 14:03:33 -0300

    feat: create server structure

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import Fastfy, { fastify } from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn"],
+});
+
+async function bootstrap() {
+  const app = Fastfy({
+    logger: true,
+  });
+
+  await app.register(cors, {
+    origin: true,
+  });
+
+  app.get("/", async (request, reply) => {
+    const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  await app.listen({ port: 3333, host: "0.0.0.0" });
+}
+
+bootstrap();
